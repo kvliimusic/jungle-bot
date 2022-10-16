20e297cfe6310e2c9d8e6ecdbf39aeaea42398b1
Jexanti <github.cytopathogenicity@simplelogin.fr>
2022-10-16 23:20:29 +0100

    v1.1 commit 3
    updated the challenge, and made the code a little more readable

--- commands/challenge.js
+++ commands/challenge.js
@@ -7,10 +7,11 @@ module.exports = {
 	.setDescription('View the weekly challenge.'),
 	async execute(interaction) {
 		console.log('challenge.js running...')
+		const challengeText = "Create an original cinematic track for a film trailer (which can be either imaginary or pre-existing). If it already exists, mute the original music."
 		const embed = {
 			title: "This week's challenge:",
 			color: 0x39f79e,
-			description: "Try a new plugin using a new instrument or effect that you don't have in ableton live. Try to combine your own sounds with the plugin to create something original!\n\nOnce you're done, submit here:\n"+config.challengeLink
+			description: challengeText+"\n\nOnce you're done, submit here:\n"+config.challengeLink
 		}
 		console.log('successfully reached end of file, returning data')
 		return interaction.reply({ embeds: [embed] });
