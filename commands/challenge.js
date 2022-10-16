810045a874cc53a0cdec8ea36891e5eddbf56baa
Jexanti <github.cytopathogenicity@simplelogin.fr>
2022-10-16 23:13:19 +0100

    1.1 commit 1
    merged challenge commands into one

--- commands/challenge.js
+++ commands/challenge.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-
+const config = require ('../config.json')
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,7 +10,7 @@ module.exports = {
 		const embed = {
 			title: "This week's challenge:",
 			color: 0x39f79e,
-			description: "Try a new plugin using a new instrument or effect that you don't have in ableton live. Try to combine your own sounds with the plugin to create something original!"
+			description: "Try a new plugin using a new instrument or effect that you don't have in ableton live. Try to combine your own sounds with the plugin to create something original!\n\nOnce you're done, submit here:\n"+config.challengeLink
 		}
 		console.log('successfully reached end of file, returning data')
 		return interaction.reply({ embeds: [embed] });
